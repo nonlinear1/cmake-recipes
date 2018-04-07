511d8d7e335f23897f514c2cbe67aa7d22a80be1
Radovan Bast <bast@users.noreply.github.com>
2018-04-07 15:56:41 +0200

    auto-ize example

--- chapter-03/recipe-05/cxx-example/example.cpp
+++ chapter-03/recipe-05/cxx-example/example.cpp
@@ -8,21 +8,21 @@ int main(int argc, char *argv[]) {
   std::cout << "number of threads: " << omp_get_max_threads()
             << std::endl;
 
-  long int n = atol(argv[1]);
+  auto n = atol(argv[1]);
   std::cout << "we will form sum of numbers from 1 to " << n << std::endl;
 
   // start timer
-  double t0 = omp_get_wtime();
+  auto t0 = omp_get_wtime();
 
-  long int s = 0;
+  auto s = 0LL;
 #pragma omp parallel for reduction(+:s)
-  for (size_t i = 1; i <= n; i++)
+  for (auto i = 1; i <= n; i++)
   {
     s += i;
   }
 
   // stop timer
-  double t1 = omp_get_wtime();
+  auto t1 = omp_get_wtime();
 
   std::cout << "sum: " << s << std::endl;
   std::cout << "elapsed wall clock time: " << t1 - t0 << " seconds" << std::endl;
